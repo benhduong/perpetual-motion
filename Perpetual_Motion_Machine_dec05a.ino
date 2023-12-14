b3c68564bfd7a281a2f73f9f90fa0fe90741885e
Ben Duong <77896857+benhduong@users.noreply.github.com>
2023-12-13 19:50:30 -0500

    Update Perpetual_Motion_Machine_dec05a.ino

--- Perpetual_Motion_Machine_dec05a.ino
+++ Perpetual_Motion_Machine_dec05a.ino
@@ -1,4 +1,3 @@
-#include "arduino_secrets.h"
 #include "thingProperties.h"
 #include "pitches.h"
 
@@ -44,6 +43,40 @@ void setup() {
 
   servo.attach(servoPin);
   pinMode(buzzer, OUTPUT);
+  
+  setupWatchdog();
+
+  // init serial monitor
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("system ready");
+}
+
+// Note: code taken from lab 4
+void setupWatchdog() {
+  // Clear and enable WDT
+  NVIC_DisableIRQ(WDT_IRQn);
+  NVIC_ClearPendingIRQ(WDT_IRQn);
+  NVIC_SetPriority(WDT_IRQn, 0);
+  NVIC_EnableIRQ(WDT_IRQn);
+
+  // Configure and enable WDT GCLK:
+  GCLK->GENDIV.reg = GCLK_GENDIV_DIV(4) | GCLK_GENDIV_ID(5);
+  while (GCLK->STATUS.bit.SYNCBUSY);
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_DIVSEL | GCLK_GENCTRL_ID(5) | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC(3);
+  while(GCLK->STATUS.bit.SYNCBUSY);
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_GEN(5) | GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_ID(3);
+
+  // Configure and enable WDT:
+  // use WDT->CONFIG.reg, WDT->EWCTRL.reg, WDT->CTRL.reg
+  WDT->CONFIG.reg = WDT_CONFIG_PER(9);
+  WDT->EWCTRL.reg = WDT_EWCTRL_EWOFFSET(8);
+  WDT->CTRL.reg = WDT_CTRL_ENABLE;
+  while(WDT->STATUS.bit.SYNCBUSY);
+}
+
+void petWatchdog() {
+  WDT->CLEAR.reg = 0xA5;
 }
 
 void loop() {
@@ -72,6 +105,8 @@ void loop() {
         ballCount = 0;
       }
     }
+    
+    petWatchdog();
   }
 }
 
@@ -108,12 +143,4 @@ void onPerpetualMotionMachineChange()  {
     delay(1000);
     detachInterrupt(digitalPinToInterrupt(sensor));
   }
-}
-
-/*
-  Since Melody is READ_WRITE variable, onMelodyChange() is
-  executed every time a new value is received from IoT Cloud.
-*/
-void onMelodyChange()  {
-  
 }
